fd0fcbc75202c9b215b35b6b7444703c2b9c3cc1
summerCat <583188551@qq.com>
2019-10-06 14:25:36 +0800

    helloword转发

--- gateway-api/src/main/java/com/linjingc/gatewayapi/GatewayRoutes.java
+++ gateway-api/src/main/java/com/linjingc/gatewayapi/GatewayRoutes.java
@@ -1,22 +1,22 @@
-package com.linjingc.gatewayapi;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 代码化router
- */
-@Configuration
-public class GatewayRoutes {
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r ->
-                        r.path("/helloword")
-                                .uri("http://localhost:9090/helloword")
-                )
-                .build();
-    }
-}
+//package com.linjingc.gatewayapi;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 代码化router
+// */
+//@Configuration
+//public class GatewayRoutes {
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r ->
+//                        r.path("/helloword")
+//                                .uri("http://localhost:9090/helloword")
+//                )
+//                .build();
+//    }
+//}
