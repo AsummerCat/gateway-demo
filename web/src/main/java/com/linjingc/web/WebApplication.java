f152fc0744bf172a7824b3860a6f0310ecaaf882
chenxiaocehng <583188551@qq.com>
2019-10-08 14:51:20 +0800

    添加了注册中心

--- web/src/main/java/com/linjingc/web/WebApplication.java
+++ web/src/main/java/com/linjingc/web/WebApplication.java
@@ -2,6 +2,7 @@ package com.linjingc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class WebApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
 
-    @RequestMapping("helloword")
-    public String helloworld(HttpServletRequest request){
-        return "helloword";
-    }
+	@RequestMapping("helloword")
+	public String helloworld(HttpServletRequest request) {
+		return "helloword";
+	}
 }
